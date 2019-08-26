6cb78dd0a73224c17ede5852c60641198e6d28b6
ip11 <iprash011@gmail.com>
2019-08-26 19:58:13 +0530

    h

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'hacking school' });
+  res.render('index', { title: 'prash' });
 });
 
 module.exports = router;

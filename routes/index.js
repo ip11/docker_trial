eee27929f1f13d225288747f718a2cd4ecf127b9
prashanth <39377668+ip11@users.noreply.github.com>
2019-08-26 18:46:48 +0530

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'teja' });
+  res.render('index', { title: 'cocoa' });
 });
 
 module.exports = router;

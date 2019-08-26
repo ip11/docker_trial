9a3cad150869f41e2c8e4b85936dda503e798ef8
prashanth <39377668+ip11@users.noreply.github.com>
2019-08-26 18:40:45 +0530

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'TheHackingSchool' });
+  res.render('index', { title: 'prash' });
 });
 
 module.exports = router;

a377c2cabae8d903024f129bbc90bec9234628cf
prashanth <39377668+ip11@users.noreply.github.com>
2019-08-26 18:55:26 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,6 +1,7 @@
-ssh -T root@35.226.147.119 <<EOF        
- cd /var/lib/jenkins/workspace/NodeApp  
- git pull       
+ssh -T root@35.226.147.119 <<EOF
+ cd /var/lib/jenkins/workspace/NodeApp
+ git pull
+ git add --chmod=+x "script.sh"
  pm2 restart all
  exit       
 EOF
